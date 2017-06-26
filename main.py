06ea92eb051faf35a99d7cb586570f8fa84e3c8f
Quanyong Huang <quanyongh@foxmail.com>
2017-06-26 21:31:55 +0800

    mod: daily

--- main.py
+++ main.py
@@ -10,25 +10,38 @@ Description:
 ----------------------------------------------------------------------------"""
 
 import pytweann.pytweann
+import random
 
-XOR = [[1, -1, 1], [1, 1, -1], [-1, -1, 1], [-1, 1, -1]]
+XOR = [[1, 0, 1], [1, 1, 0], [0, 0, 0], [0, 1, 1]]
 AND = [[1, 1, 1], [1, -1, -1], [-1, 1, -1], [-1, -1, -1]]
+
+XOR_Sample = []
+for i in range(4):
+	XOR_Sample.extend(XOR)
+
+random.shuffle(XOR_Sample)
+
+XOR_test = []
+XOR_test.extend(XOR)
+
+random.shuffle(XOR_test)
 
 
 def main():
-    environment = pytweann.pytweann.create_environment(input_size=2, output_size=1)
-    while True:
-        for genome in environment.get_genomes():
-            fitness = 0
-            neuron_network = genome.get_neuron_network()
-            for data in AND:
-                fitness += 1 if neuron_network.evaluate(data[:-1])[0] > 0 and data[-1] > 0 else 0
-            if fitness == 4:
-                print("get!")
-
-            genome.fitness = fitness
-        environment.new_generation()
+	environment = pytweann.pytweann.create_environment(input_size=2, output_size=1)
+	while True:
+		for genome in environment.get_genomes():
+			fitness = 0
+			neuron_network = genome.get_neuron_network()
+			for data in XOR_Sample:
+				output = neuron_network.evaluate(data[:-1])[0]
+				fitness += 1 - (output - data[-1]) ** 2
+			genome.fitness = fitness
+			c = 0
+			if genome.fitness > 15.5:
+				print("get")
+		environment.new_generation()
 
 
 if __name__ == '__main__':
-    main()
+	main()
--- pytweann/neuron_network.py
+++ pytweann/neuron_network.py
@@ -13,75 +13,84 @@ import math
 
 
 def sigmoid(x):
-    return 2 / (1 + math.exp(-4.9 * x)) - 1
+	return 2 / (1 + math.exp(-4.9 * x)) - 1
 
 
 class Neuron(object):
-    def __init__(self):
-        self.incoming_link = {}
-        self.value = 0
+	def __init__(self):
+		self.incoming_link = {}
+		self.value = 0
 
-    def add_incoming(self, gene):
-        self.incoming_link[gene.from_node] = gene.weight
+	def add_incoming(self, gene):
+		self.incoming_link[gene.from_node] = gene.weight
 
 
 class NeuronNetwork(object):
-    def __init__(self, genome):
+	def __init__(self, genome):
 
-        self.genome = genome
+		self.genome = genome
 
-        self.input_size = self.genome.input_size
-        self.output_size = self.genome.output_size
-        self.hidden_size = self.genome.hidden_size
+		self.input_size = self.genome.input_size
+		self.output_size = self.genome.output_size
+		self.hidden_size = self.genome.hidden_size
 
-        self.neurons = []
+		self.neurons = []
 
-        for i in range(self.input_size + self.output_size + self.hidden_size + 1):
-            self.neurons.append(Neuron())
+		for i in range(self.input_size + self.output_size + self.hidden_size + 1):
+			self.neurons.append(Neuron())
 
-        for gene in genome.genes:
-            self.neurons[gene.to_node].add_incoming(gene)
+		for gene in genome.genes:
+			if gene.enable:
+				self.neurons[gene.to_node].add_incoming(gene)
 
-    def evaluate(self, inputs):
-        assert len(inputs) == self.input_size, "wrong input data size."
+	def evaluate(self, inputs):
+		assert len(inputs) == self.input_size, "wrong input data size."
 
-        self.neurons[0].value = 1
+		self.neurons[0].value = 1
 
-        for i, data in enumerate(inputs):
-            self.neurons[i + 1].value = data
+		for i, data in enumerate(inputs):
+			self.neurons[i + 1].value = data
 
-        for neuron in self.neurons:
-            if neuron.incoming_link:
-                value = 0
-                for i, weight in neuron.incoming_link.items():
-                    value += self.neurons[i].value * weight
-                neuron.value = sigmoid(value)
+		for i in range(self.hidden_size):
+			neuron = self.neurons[self.input_size + self.output_size + 1 + i]
+			self.evaluate_neuron(neuron)
 
-        return tuple(map(lambda x: x.value, self.neurons[self.input_size + 1:self.input_size + self.output_size + 1]))
+		for i in range(self.output_size):
+			neuron = self.neurons[self.input_size + 1 + i]
+			self.evaluate_neuron(neuron)
+
+		return tuple(map(lambda x: x.value, self.neurons[self.input_size + 1:self.input_size + self.output_size + 1]))
+
+	def evaluate_neuron(self, neuron):
+		if neuron.incoming_link:
+			value = 0
+			for i, weight in neuron.incoming_link.items():
+				value += self.neurons[i].value * weight
+			neuron.value = sigmoid(value)
 
 
 def main():
-    from genome import Genome, Gene
-    from environment import Environment
-    from config import new_config
-    config = new_config()
-    config["input_size"] = 2
-    config["output_size"] = 1
-    environment = Environment(config)
-    genome = Genome(environment)
-    gene1 = Gene()
-    gene1.from_node = 1
-    gene1.to_node = 3
-    gene1.weight = 1
-    gene2 = Gene()
-    gene2.from_node = 2
-    gene2.to_node = 3
-    gene2.weight = 2
-    genome.genes.append(gene1)
-    genome.genes.append(gene2)
-    neron_network = NeuronNetwork(genome)
-    print(neron_network.evaluate([1, -0.1]))
+	from genome import Genome, Gene
+	from environment import Environment
+	from config import new_config
+	config = new_config()
+	config["input_size"] = 2
+	config["output_size"] = 1
+	environment = Environment(config)
+	genome = Genome(environment)
+	gene1 = Gene()
+	gene1.from_node = 1
+	gene1.to_node = 3
+	gene1.weight = 1
+	gene2 = Gene()
+	gene2.from_node = 2
+	gene2.to_node = 3
+	gene2.weight = 2
+	genome.genes.append(gene1)
+	genome.genes.append(gene2)
+	neron_network = NeuronNetwork(genome)
+	print(neron_network.evaluate([1, -0.1]))
 
 
 if __name__ == '__main__':
-    main()
+	main()
